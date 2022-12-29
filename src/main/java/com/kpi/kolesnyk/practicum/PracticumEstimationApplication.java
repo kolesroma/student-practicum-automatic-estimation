9cefd0488dadf07b16733bada5838b8053f51426
Roma Kolesnyk <romakoles1@ukr.net>
2022-12-29 13:48:00 +0200

    init structure; pom dependencies

---
+++ src/main/java/com/kpi/kolesnyk/practicum/PracticumEstimationApplication.java
@@ -0,0 +1,11 @@
+package com.kpi.kolesnyk.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticumEstimationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PracticumEstimationApplication.class);
+    }
+}
