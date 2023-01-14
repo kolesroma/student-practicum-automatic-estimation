2ab270a7c781cb220da0ed3e6ff58c1e4de227a6
Roma Kolesnyk <romakoles1@ukr.net>
2023-01-15 00:24:18 +0200

    add access for teacher role

--- src/main/java/com/kpi/kolesnyk/practicum/controller/TaskRestController.java
+++ src/main/java/com/kpi/kolesnyk/practicum/controller/TaskRestController.java
@@ -2,6 +2,7 @@ package com.kpi.kolesnyk.practicum.controller;
 
 import com.kpi.kolesnyk.practicum.dto.TaskCreationDto;
 import com.kpi.kolesnyk.practicum.service.TaskService;
+import com.kpi.kolesnyk.practicum.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.security.Principal;
 
+import static com.kpi.kolesnyk.practicum.exception.ExceptionSupplier.*;
+
+
 @RestController
 @RequiredArgsConstructor
 public class TaskRestController {
     private final TaskService taskService;
+    private final UserService userService;
 
     @PostMapping(value = "/tasks/create")
     public void create(@Valid @RequestBody TaskCreationDto task,
                        Principal principal) {
-        System.out.println(task);
-        taskService.create(task, principal);
+        var user = userService.findByUsername(principal.getName());
+        if ("ROLE_TEACHER".equals(user.getRole().getAuthority())) {
+            taskService.create(task, principal);
+        } else {
+            NO_ACCESS.get();
+        }
     }
 }
